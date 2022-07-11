4dae9a524b31a4ad8181e9fb7994ee04c9acc5ab
Jordan Altomare <jaltomar@u.rochester.edu>
2022-07-11 08:55:31 -0400

    thank you page colors added

--- src/components/User/ThankYou.js
+++ src/components/User/ThankYou.js
@@ -6,13 +6,15 @@ import dog from "./DogImages/wink.png";
 function ThankYou() {
   return (
     <>
+    <div className="container-fluid userHtml" id="noPadding">
       <UserNav />
-      <Container style={{ marginTop: "40px", textAlign: "center" }}>
+      <div className="container text-center themeBlue" id="extra3Padding">
         <h2>
           Thank you for your application! We will be in touch with you soon!
         </h2>
         <img src={dog} alt="Cute Puppy" style={{ width: "50%" }} />
-      </Container>
+      </div>
+    </div>
     </>
   );
 }
