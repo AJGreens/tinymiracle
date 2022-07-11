f24873f27d4fe29593875ef3d88f6f53296579e6
Jordan Altomare <jaltomar@u.rochester.edu>
2022-07-11 17:27:15 -0400

    fixed typos on homepage

--- src/components/User/Home.js
+++ src/components/User/Home.js
@@ -53,7 +53,7 @@ function Home() {
               <Col>
                 <h2>Our Inspiration</h2>
                 <p className="text-justify tan">
-                  Kim Rutherford is a lifelong animal lover, who’s caring and
+                  Kim Rutherford is a lifelong animal lover, whose caring and
                   kindness to all creatures is well known in the community. She
                   was able to turn her passion for animals into a business that
                   she truly loves. Kim’s farm is an eclectic mix of furry and
@@ -66,11 +66,11 @@ function Home() {
                 <h2>Our History</h2>
                 <p className="text-justify tan">
                   Kim wanted to help animals by finding them their forever
-                  homes. She started her rescue in 2011. Every rescue becomes
+                  homes. She started her rescue in 2011. Every rescue animal becomes
                   her child, just like her pet care kids. No matter the
                   situation, Kim wants to save every soul possible. She wanted
-                  to save as many animals from kill shelters and puppy mills, as
-                  possible. If she could she would take in every single dog, who
+                  to save as many animals from kill shelters and puppy mills as she could.
+                  If she could she would take in every single dog, who
                   needs help. The dogs that are lucky enough to be saved by Kim,
                   stay with her until they are ready to find a forever home.
                   When the match between dog and family are perfect, Kim makes
